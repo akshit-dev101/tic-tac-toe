8dc4e271415e1c36b906cc4729180466725cc314
Akshit <akshitkumar540@gmail.com>
2021-09-12 17:17:55 +0530

    added final logic

---
+++ src/components/StatusMessage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StatusMessage = ({ winner, current }) => {
+    const noMovesLeft = current.board.every((el) => el !== null)
+
+    return (
+        <div>
+            {winner && `Winner is ${winner}`}
+                {!winner && !noMovesLeft && `Next player is ${current.isXNext ? 'X' : 'O'}`}
+            {!winner && noMovesLeft && "X and O tied"}
+        </div>
+    )
+}
+
+export default StatusMessage
